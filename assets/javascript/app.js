82825aa81696fca41b9233eee464e89d9391a2b7
IDGB <49776938+ivergil@users.noreply.github.com>
2019-06-16 15:16:31 -0600

    Adds Buttons

---
+++ assets/javascript/app.js
@@ -0,0 +1,28 @@
+var food = ["Sushi", "Hot Dog", "Pizza", "Coockies"];
+
+function displayButtons() {
+    $(".buttons-holder").empty();
+
+    for (var i = 0; i < food.length; i++) {
+        var newBtn = $("<button>");
+        newBtn.addClass("btn, btn-primary m-2");
+        // Adding a data-attribute
+        // newBtn.attr("name-data", food[i]);
+        // Providing the initial button text
+        newBtn.text(food[i]);
+        // console.log(newBtn);
+        // Adding the button to the buttons-view div
+        $(".buttons-holder").append(newBtn);
+    }
+}
+
+$("#add-gif").on("click", function (e) {
+    e.preventDefault();
+    var newFood = $("#food-input").val().trim();
+    food.push(newFood);
+    console.log(newFood);
+
+    displayButtons();
+});
+
+
